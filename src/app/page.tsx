4ac4b6b237078a0b1f9fb653704cbacb64552a93
codec-xyz <112908328+codec-xyz@users.noreply.github.com>
2024-05-12 21:54:19 -0400

    Removes t3 demo pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,33 @@
-import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-	const hello = await api.post.hello({ text: "from tRPC" });
 	const { userId } = auth();
 
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+	return <div className="w-full h-screen flex flex-col overflow-auto">
+		<div className="flex gap-4 py-4 px-8 items-center">
+			<div className="text-3xl font-bold">Log</div>
 			<Link href="/app">App</Link>
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank"
-					>
-						<h3 className="text-2xl font-bold">First Steps →</h3>
-						<div className="text-lg">
-							Just the basics - Everything you need to know to set up your
-							database and authentication.
-						</div>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank"
-					>
-						<h3 className="text-2xl font-bold">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to
-							deploy it.
-						</div>
-					</Link>
-				</div>
-				<div className="flex flex-col items-center gap-2">
-					<p className="text-2xl text-white">
-						{hello ? hello.greeting : "Loading tRPC query..."}
-					</p>
-
-					<div className="flex flex-col items-center justify-center gap-4">
-						<p className="text-center text-2xl text-white">
-							{userId && <span>Logged in as {userId}</span>}
-						</p>
-						<SignedOut>
-							<SignInButton>
-								<button className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
-									Sign In
-								</button>
-							</SignInButton>
-							<SignUpButton>
-								<button className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
-									Sign Up
-								</button>
-							</SignUpButton>
-						</SignedOut>
-						<SignedIn>
-							<SignOutButton>
-								<button className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
-									Sign Out
-								</button>
-							</SignOutButton>
-						</SignedIn>
-					</div>
-				</div>
-
-				<CrudShowcase />
-			</div>
-		</main>
-	);
-}
-
-async function CrudShowcase() {
-	return null;
-
-	const latestPost = await api.post.getLatest();
-
-	return (
-		<div className="w-full max-w-xs">
-			{latestPost ? (
-				<p className="truncate">Your most recent post: {latestPost.name}</p>
-			) : (
-				<p>You have no posts yet.</p>
-			)}
-
-			<CreatePost />
+			<Link href="/app">Github</Link>
+			<div className="flex-grow"></div>
+			<SignedOut>
+				<SignInButton>
+					<button className="rounded border-slate-400 px-4 py-2 transition hover:bg-slate-200">
+						Sign In
+					</button>
+				</SignInButton>
+				<SignUpButton>
+					<button className="rounded bg-slate-200 px-4 py-2 transition hover:bg-white/20">
+						Sign Up
+					</button>
+				</SignUpButton>
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
 		</div>
-	);
+	</div>;
 }
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { dbRouter } from "./routers/db";
 
@@ -8,7 +7,6 @@ import { dbRouter } from "./routers/db";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-	post: postRouter,
 	db: dbRouter,
 });
 
